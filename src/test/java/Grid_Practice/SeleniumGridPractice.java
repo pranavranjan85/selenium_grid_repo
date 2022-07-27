4b1c759e5a8e7c4b6b8df516361cd9b0d5076ab9
pranavranjankr85 <pranavranjankr@gmail.com>
2022-07-27 16:20:38 +0530

    this commit

---
+++ src/test/java/Grid_Practice/SeleniumGridPractice.java
@@ -0,0 +1,27 @@
+package Grid_Practice;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class SeleniumGridPractice 
+{
+	@Test
+	public void gridPractice() throws MalformedURLException
+	{
+		URL url=new URL("http://localhost:4444");
+		
+		DesiredCapabilities cap= new DesiredCapabilities();
+		cap.setBrowserName("chrome");
+		cap.setPlatform(Platform.WINDOWS);
+		
+		RemoteWebDriver driver= new RemoteWebDriver(url, cap);
+		
+		driver.get("https://www.google.com/");
+	}
+
+}
